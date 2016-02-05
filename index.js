1be05c1008b8d8d8b426b9e17176e1716c2bd384
Jonathan M. Altman <jonathan@racegogo.com>
2016-02-05 21:28:54 +0000

    Initial functionality: load routes from file tree
    Initial functionality built.  We can now load routes from modules
    living in .js files in a directory (and subdirectories thereof)
    specified in the configuration given to us, or './endpoints' by
    default if non is specified in the configuration.  We load each
    module's specifically defined function and add a route to the
    restify instance using the method, route, and handler provided.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
+let loadRoutes = require('./lib/load_modules'),
+    _ = require('highland');
 
-module.exports = function(restify_server, conf) {
-	return {};
+
+module.exports = function(restifyServer, conf, params, cb) {
+  conf.handlersDir = conf.handlersDir || './endpoints';
+  loadRoutes(conf.handlersDir, restifyServer, params, cb);
 };
