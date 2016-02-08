ebd8e33393de6cccf7846af252a26d99d05f5709
Tyler Franklin <tyler@tri.ad>
2016-02-07 21:33:38 -0500

    move some of these tests to index, mimic module structure for clarity

--- spec/index.js
+++ spec/index.js
@@ -1,5 +1,33 @@
 'use strict';
 
-describe('ak-api', () => {
-  require('./tests/structure');
+let mocha = require('mocha'),
+    chai = require('chai'),
+    restify = require('restify'),
+    RestifyServer = require('restify/lib/server'),
+    entrypoint = require('../index');
+
+const expect = chai.expect,
+    assert = chai.assert,
+
+    testServer = restify.createServer({name: 'testServer'}),
+    moduleType = typeof entrypoint;
+
+describe('AK-API', () => {
+
+  // index.js structure and behavior
+  describe('entrypoint', () => {
+
+    it('should be a function', () => {
+      expect(moduleType).to.equal('function');
+    });
+
+    it('should throw an error if invoked without arguments', () => {
+      expect(entrypoint).to.throw();
+    });
+  });
+
+  // lib folder unit structure and behavior
+  describe('submodule', () => {
+    require('./lib/load_modules');
+  });
 });
